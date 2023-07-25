bdcc9dd5a6980eed0ab6288d56aa00c6027c9960
Dinh Duy Phuoc <ddphuoc19@apcs.fitus.edu.vn>
2023-07-25 10:36:11 +0700

    Add client source code

--- client/src/utils/utils.js
+++
@@ -1,9 +0,0 @@
-export const convertISO8601ToMilliseconds = (iso8601, zone = 0) => {
-  const milli = 60 * 60 * 1000;
-  const time = new Date(iso8601).getTime() + zone * milli;
-  return time;
-};
-
-export const roundNum = (number, precision = 2) => {
-  return Math.round(number * Math.pow(10, precision)) / Math.pow(10, precision);
-};
